e53b0ea8afd22e06132974a844265faaf7ff84d4
Dj <donald.ballard@hotmail.com>
2014-12-12 17:25:07 -0800

    verifier and token found, need to practice request

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session')
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -24,17 +25,22 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+    secret: 'something',
+    resave: false,
+    saveUninitialized: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+// app.use(function(req, res, next) {
+//     var err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
 
 // error handlers
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var OAuth = require('oauth');
+var OAuth = require('oauth').OAuth;
 // var request = require('request');
 // var OAuth   = require('oauth-1.0a');
 
@@ -9,12 +9,21 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Buddha Bar: Online Store Manager' });
 });
 
+function getAuthToken(req, res, oauth) {
+    oauth.getOAuthRequestToken(function(err, token, token_secret, result) {
+         if (err) return console.log( err );
 
-router.get('/test', function(req, res){
-     // var OAuth = require('oauth');
-    var domain = 'http://localhost:3000';
+        req.session.oauth = {};
+        req.session.oauth.token = token;
+        req.session.oauth.token_secret = token_secret;
+        
+        // redirect the etsy url
+        res.redirect( result[ "login_url" ] );
+    });
+}
 
-    var oauth = new OAuth.OAuth(
+ var domain = 'http://localhost:3000';
+ var oauth = new OAuth(
       'https://openapi.etsy.com/v2/oauth/request_token',
       'https://openapi.etsy.com/v2/oauth/access_token',
       'glv40yg7ycl6czknj4f9v0xw',
@@ -22,132 +31,60 @@ router.get('/test', function(req, res){
       '1.0',
       domain + '/auth/etsy/callback',
       'HMAC-SHA1'
-    );
-    
-    oauth.getOAuthRequestToken(function(err, token, token_secret, result) {
-        console.log(arguments);
-    });
+);
+ // function to test data, 
 
-    // oauth.get(
-    //   'https://openapi.etsy.com/v2/listings/:listing_id',
-    //   'your user token for this app', //test user token
-    //   'your user secret for this app', //test user secret            
-    //   function (e, data, res){
-    //     if (e) console.error(e);        
-    //     console.log(require('util').inspect(data));    
-    //   });    
+// function testGettingUserProfile(token, token_secret, callback) {
+//     oauth.get('/users/dj', function(err, data) {
+//         callback(data);
+//     });
+// }
+console.log(OAuth.prototype);
+router.get('/auth/etsy/callback', function(req, res) {
+    var verifier = req.query.oauth_verifier;
+    var token = req.session.oauth.token;
+    var secret = req.session.oauth.token_secret;
+    console.log(verifier);
+    console.log(token);
+    console.log(secret);
+
+    oauth.getOAuthAccessToken(token, secret, verifier, function(err, token, token_secret, results){
+        if (err) { return res.status(500).end(); }
+        console.log(token, token_secret);
+        res.status(200);
+        // testGettingUserProfile(token, token_secret, function(data) {
+        //     res.send(data);
+        // })
+
+
+        // if (req.session.oauth) {
+        //     req.session.oauth.verifier = req.query.oauth_verifier;
+        //     var auth = req.session.ouath;
+        //     oauth.getOAuthAccessToken(
+        //         auth.token,
+        //         auth.token_secret,
+        //         auth.verifier,
+        //         function(err, token, token_secret, results){
+        //             if (err) {
+        //                 console.log(err);
+        //             }
+        //             else {
+        //                 req.session.oauth.access_token = token;
+        //                 req.session.oauth.access_token_secret = token_secret;
+        //                  if (callback) callback.call(this, req, res);
+        //             }
+        //         }
+        //     );
+        // }
+        // console.log(this);
+    });
+});
+
+
+
+router.get('/auth/etsy', function(req, res){
+
+    getAuthToken(req, res, oauth);
 
 });
-// var oauth = new OAuth({
-//     consumer: {
-//         public: 'glv40yg7ycl6czknj4f9v0xw',
-//         secret: 'w1udrm2cuk'
-//     }
-// });
-
-
-// Obtaining Temporary Credentials - request token
-// var request_data = {
-//     url: 'https://openapi.etsy.com/v2/oauth/request_token?scope=email_r',
-//     method: 'POST'
-// };
-
-// request({
-//     url:    request_data.url,
-//     method: request_data.method,
-//     form:   oauth.authorize(request_data),
-//     json:   true //parse respone as json
-// }, function(err, res, data) {
-//     // {
-//     //     login_url: 'https://www.etsy.com/oauth/signin?oauth_consumer_key=xxx&oauth_token=xxx&service=v2_prod',
-//     //     oauth_token: 'xxx',
-//     //     service: 'xxx',
-//     //     oauth_token_secret: 'xxx',
-//     //     oauth_callback_confirmed: 'true',
-//     //     oauth_consumer_key: 'xxx',
-//     //     oauth_callback: 'oob'
-//     // }
-// }
-// access the login_url to get the credential (oauth_verifier)
-
-// var request_data = {
-//     url:    'https://openapi.etsy.com/v2/oauth/access_token',
-//     method: 'POST',
-//     data: {
-//         oauth_verifier: 'xxx'
-//     }
-// };
-
-// var token = {
-//     public: 'xxx',
-//     secret: 'xxx'
-// }
-
-// request({
-//     url:    request_data.url,
-//     method: request_data.method,
-//     form:   oauth.authorize(request_data, token)
-// }, function(err, res, data) {
-//     // oauth_token=xxx&oauth_token_secret=xxx
-// });
-// access the login_url to get the credential
-
-// Obtaining Token Credentials - access token
-
-// var request_data = {
-//     url: 'https://openapi.etsy.com/v2/oauth/access_token',
-//     method: 'POST',
-//     data: {
-//         oauth_verifier: 'xxx'
-//     }
-// };
-
-// var token = {
-//     public: 'xxx',
-//     secret: 'xxx'
-// }
-
-// request({
-//     url: request_data.url,
-//     method: request_data.method,
-//     form: oauth.authorize(request_data, token)
-// }, function(err, res, data) {
-//     // oauth_token=xxx&oauth_token_secret=xxx
-// });
-// Making an Authorized Request to the API - since you got the tokens you can use oauth-request to play around with the API
-
-// var request_data = {
-//     url: 'https://openapi.etsy.com/v2/users/__SELF__',
-//     method: 'GET'
-// };
-
-// var token = {
-//     public: 'xxx',
-//     secret: 'xxx'
-// }
-
-// request({
-//     url: request_data.url,
-//     qs: oauth.authorize(request_data, token),
-//     json: true
-// }, function(err, res, data) {
-//     // {
-//     //     count: 1,
-//     //     results: [{
-//     //         user_id: 53899516,
-//     //         login_name: 'ddooo',
-//     //         primary_email: 'joeddo89@gmail.com',
-//     //         creation_tsz: 1411796074,
-//     //         referred_by_user_id: null,
-//     //         feedback_info: [Object],
-//     //         awaiting_feedback_count: 0
-//     //     }],
-//     //     params: {
-//     //         user_id: '__SELF__'
-//     //     },
-//     //     type: 'User',
-//     //     pagination: {}
-//     // }
-// });
-
 module.exports = router;
