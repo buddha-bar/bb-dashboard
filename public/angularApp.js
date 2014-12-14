7d3134cc23b4f9af73bacc49cab5f7921ce3f32f
Regan <reganlperkins@gmail.com>
2014-12-13 19:37:56 -0800

    angular test for item display

--- public/angularApp.js
+++ public/angularApp.js
@@ -1,1 +1,21 @@
-var app = angular.module('showItem', []);
+(function(){
+  var app = angular.module('dashboard', []);
+  app.controller('ItemController', function(){
+    this.item = items;
+  });
+
+  //need to loop through items in DB for now using these test objects
+  var items = [
+    {
+      name : 'name',//query to db for name goes here
+      stock : '4',//query to db
+      img : '/images/bb-logo.png'//query to db
+    }
+    {
+      name : 'name2',//query to db for name goes here
+      stock : '6',//query to db
+      img : '/images/bb-logo.png'//query to db
+    }
+  ];
+
+})();
