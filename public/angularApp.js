b7b1210ed143eaaba6b10f67bd8a74b719574e7b
Regan <reganlperkins@gmail.com>
2014-12-13 22:55:55 -0800

    add angular link and third column

--- public/angularApp.js
+++ public/angularApp.js
@@ -8,10 +8,10 @@
   //need to loop through items in DB for now using these test objects
   var items = [
     {
-      name : 'name',//query to db for name goes here
+      name : 'name1',//query to db for name goes here
       stock : '4',//query to db
       img : '/images/bb-logo.png'//query to db
-    }
+    },
     {
       name : 'name2',//query to db for name goes here
       stock : '6',//query to db
@@ -22,4 +22,4 @@
 })();
 
 
-db.test.find(); // { age: { $gt: 18 } }, { name: 1, stock: 1 } )
+//db.test.find(); // { age: { $gt: 18 } }, { name: 1, stock: 1 } )
