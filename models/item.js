a0c965b283605b9abd391ec1687808b22908dca5
Regan <reganlperkins@gmail.com>
2014-12-13 17:34:02 -0800

    connecting MongoDB

--- models/item.js
+++ models/item.js
@@ -3,7 +3,6 @@ var mongoose = require('mongoose');
 var Item = mongoose.Schema({
     name: String,
     stock: Number
-  }
 });
 
 module.exports = mongoose.model('Item', Item)
--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var User = mongoose.Schema({
     username: String,
     password: String,
-    items: [Item]
+    // items: [Item],
     credentials: {
       amazon: Object,
       etsy: Object,
