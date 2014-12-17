07775326f8a5c1690cf7367fec29afc3d9309082
Regan <reganlperkins@gmail.com>
2014-12-16 18:57:22 -0800

    css changes

--- public/scripts/menu-select.js
+++ public/scripts/menu-select.js
@@ -1,11 +1,11 @@
-(function(){
+// (function(){
   $('.checkbox').on('click', function(){
     
     if($(this).find('.figure-container div').hasClass('clicked')){
-      $(this).find('.figure-container div').removeClass('entypo-check clicked').animate('slow'); 
+      $(this).find('.figure-container div').removeClass('clicked').animate('slow'); 
     } else {
-      $(this).find('.figure-container div').addClass('entypo-check clicked').animate('slow'); 
+      $(this).find('.figure-container div').addClass('clicked').animate('slow'); 
     }
     
   });
-})();
+// })();
