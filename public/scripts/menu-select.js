0f135be48346297c1c499d63011592284b4bf4af
Regan <reganlperkins@gmail.com>
2014-12-16 19:44:25 -0800

    menue selector

--- public/scripts/menu-select.js
+++ public/scripts/menu-select.js
@@ -1,11 +1,11 @@
 // (function(){
-  $('.checkbox').on('click', function(){
+  // $('.checkbox').on('click', function(){
     
-    if($(this).find('.figure-container div').hasClass('clicked')){
-      $(this).find('.figure-container div').removeClass('clicked').animate('slow'); 
-    } else {
-      $(this).find('.figure-container div').addClass('clicked').animate('slow'); 
-    }
+  //   if($(this).find('.figure-container div').hasClass('clicked')){
+  //     $(this).find('.figure-container div').removeClass('clicked').animate('slow'); 
+  //   } else {
+  //     $(this).find('.figure-container div').addClass('clicked').animate('slow'); 
+  //   }
     
-  });
+  // });
 // })();
